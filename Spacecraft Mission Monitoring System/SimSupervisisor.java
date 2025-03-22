6940e87788a0e9dc89371a802fdc310e6e9b2ef7
ThongCoding <113760953+ThongCoding@users.noreply.github.com>
2025-03-22 00:00:38 -0700

    Create SimSupervisisor.java

---
+++ Spacecraft Mission Monitoring System/SimSupervisisor.java
@@ -0,0 +1,29 @@
+
+public class SimSupervisor extends Employee {
+    private int yearsOfExperience;
+
+    //Constructor that takes in all necessary fields
+    public SimSupervisor(int employeeID, String name, String role, String eMail, int phoneNumber, String location, int yearsOfExperience){
+        super(employeeID, name, role, eMail, phoneNumber, location);
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    //Getter for yearsOfExperience
+    public int getYearsOfExperience(){
+        return yearsOfExperience;
+    }
+
+    //Setter for yearsOfExperience
+    public void setYearsOfExperience(int yearsOfExperience){
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    // Additional method
+
+    /**
+     * Starts a simulation for the given mission.
+     */
+    public void initiateSimulation(Simulation simulation) {
+        // Logic to initialize simulation
+    }
+}
