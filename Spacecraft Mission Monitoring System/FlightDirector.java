799fe5c8491956e3380c4dd9c7de5eeae4e62992
wizardsonia333 <soniapandeyg@gmail.com>
2025-03-22 00:00:11 -0700

    Update FlightDirector.java

--- Spacecraft Mission Monitoring System/FlightDirector.java
+++ Spacecraft Mission Monitoring System/FlightDirector.java
@@ -1,20 +1,31 @@
-
 public class FlightDirector extends Employee {
-    private int authorityLevel;
+    private int yearsOfExperience;
 
-    //Constructor that takes in all necessary fields
-    public FlightDirector(int employeeID, String name, String role, String eMail, int phoneNumber, String location, int authorityLevel){
+    // Constructor that takes in all necessary fields
+    public FlightDirector(int employeeID, String name, String role, String eMail, int phoneNumber, String location, int yearsOfExperience){
         super(employeeID, name, role, eMail, phoneNumber, location);
-        this.authorityLevel = authorityLevel;
+        this.yearsOfExperience = yearsOfExperience;
     }
 
-    //Getter for authorityLevel
-    public int getAuthorityLevel(){
-        return authorityLevel;
+    // Getter for yearsOfExperience
+    public int getYearsOfExperience(){
+        return yearsOfExperience;
     }
 
-    //Setter for authorityLevel
-    public void setAuthorityLevel(int authorityLevel){
-        this.authorityLevel = authorityLevel;
+    // Setter for yearsOfExperience
+    public void setYearsOfExperience(int yearsOfExperience){
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    // Schedules a future maneuver for a given mission
+    // Parameters: missionID and a Maneuver object with future execution details
+    public void scheduleFutureManeuver(String missionID, Maneuver futureManeuver) {
+        // TODO: Add logic to add a scheduled maneuver to the mission
+    }
+    
+    // Schedules an immediate maneuver for a given mission
+    // Parameters: missionID and a Maneuver object with immediate execution details
+    public void scheduleImmediateManeuver(String missionID, Maneuver immediateManeuver) {
+        // TODO: Add logic to notify external system and log this as an immediate maneuver
     }
 }
