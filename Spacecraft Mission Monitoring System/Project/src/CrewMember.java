6c78f65be6085c6103c2bd99caec6225d33946bd
Liam <liamnguyen224@gmail.com>
2025-05-08 22:08:34 -0700

    adding 2 classes: CrewCommander and CrewMember

---
+++ Spacecraft Mission Monitoring System/Project/src/CrewMember.java
@@ -0,0 +1,17 @@
+public class CrewMember extends Employee {
+    private int crewID;
+
+    public CrewMember(int employeeID, String name, String userName, String password,
+            String workEmail, int phoneNumber, String location, int crewID) {
+        super(employeeID, name, userName, password, workEmail, phoneNumber, location);
+        this.crewID = crewID;
+    }
+
+    public int getCrewID() {
+        return crewID;
+    }
+
+    public void setCrewID(int crewID) {
+        this.crewID = crewID;
+    }
+}
